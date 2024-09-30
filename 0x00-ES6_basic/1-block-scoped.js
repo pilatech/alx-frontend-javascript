3a132c5625831c75d1e5045824551cc5100e2266
Pilate <pilatech6@gmail.com>
2024-09-30 17:58:30 +0200

    fix_2 #1 of 0x00

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,10 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+  let task = false;
+  let task2 = true;
 
   if (trueOrFalse) {
-    /* eslint-disable no-unused-vars, prefer-const */
-    let task = true;
-    let task2 = false;
+    task = false;
+    task2 = true;
   }
 
   return [task, task2];
