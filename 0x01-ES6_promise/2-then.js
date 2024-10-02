b4c75fd7202dd745599cf199d9df642f42ca8def
Pilate <pilatech6@gmail.com>
2024-10-02 19:56:39 +0200

    fix_3 #2 of 0x01

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
-    status: 200,
-    body: 'success',
-  })).catch(() => new Error()).finally(() => {
+  let retVal = null;
+  promise.then(() => {
+    retVal = {
+      status: 200,
+      body: 'success',
+    };
+  }).catch(() => {
+    retVal = new Error();
+  }).finally(() => {
     console.log('Got a response from the API');
   });
+  return retVal;
 }
