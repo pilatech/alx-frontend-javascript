8e0a0fe5fefb356dff588afd86b709da0bb88fd9
Pilate <pilatech6@gmail.com>
2024-10-09 04:43:33 +0200

    fix_2 on #8 of 0x03

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,11 @@
 export default function cleanSet(set, startString) {
   if (!startString) return '';
-  const starr = Array.from(set);
+  const starr = [];
+  for (const item of set) {
+    starr.push(item);
+  }
+  if (starr.length === 0) return '';
+
   const cleanedArr = starr.filter((value) => value.startsWith(startString));
 
   const arrWithoutFirst = cleanedArr.map((item) => item.substring(startString.length));
